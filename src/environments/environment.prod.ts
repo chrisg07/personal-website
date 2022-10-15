e478a9a80560e4d31b2de60be988b2a9ad097261
Christopher Guilliams <chrisg07@vt.edu>
2022-10-15 18:13:08 -0500

    initialized firebase deployment and hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'personal-website-86be6',
+    appId: '1:763143449088:web:b4e94b7bda98d9afee06e5',
+    storageBucket: 'personal-website-86be6.appspot.com',
+    apiKey: 'AIzaSyCenIiH_i3W2hUnqEPqDkwl7A9A1y2CFXM',
+    authDomain: 'personal-website-86be6.firebaseapp.com',
+    messagingSenderId: '763143449088',
+  },
   production: true
 };
